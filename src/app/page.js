6411624b8ca40bc7678973d2694637124a3d3202
julia85310 <juliarueda85310@gmail.com>
2024-11-25 10:01:28 +0100

    Iniciado del boletin

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,72 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import Ejercicio1 from './StudentList';
+import Ejercicio2 from './LimitedCounter';
+import Ejercicio3 from './AverageCalculator';
+import Ejercicio4 from './SurveySimulator';
+import Ejercicio5 from './ShoppingList';
+import Ejercicio6 from './LoginForm';
+import Ejercicio7 from './ImageGallery';
+import Ejercicio8 from './MultiCounter';
+import Ejercicio9 from './UserTable';
+import Ejercicio10 from './GuessTheNumber';
+import Ejercicio11 from './TimeCounter';
+import Ejercicio12 from './ThemeSelector';
+import Ejercicio1314 from './LiveValidationForm';
+import Ejercicio15 from './TextCarouse1';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <h1>Ejercicio 1</h1>
+      <Ejercicio1/>
+      <Separacion/> 
+      <h1>Ejercicio 2</h1>
+      <Ejercicio2/>
+      <Separacion/> 
+      <h1>Ejercicio 3</h1>
+      <Ejercicio3/>
+      <Separacion/> 
+      <h1>Ejercicio 4</h1>
+      <Ejercicio4/>
+      <Separacion/> 
+      <h1>Ejercicio 5</h1>
+      <Ejercicio5/>
+      <Separacion/> 
+      <h1>Ejercicio 6</h1>
+      <Ejercicio6/>
+      <Separacion/> 
+      <h1>Ejercicio 7</h1>
+      <Ejercicio7/>
+      <Separacion/> 
+      <h1>Ejercicio 8</h1>
+      <Ejercicio8/>
+      <Separacion/> 
+      <h1>Ejercicio 9</h1>
+      <Ejercicio9/>
+      <Separacion/> 
+      <h1>Ejercicio 10</h1>
+      <Ejercicio10/>
+      <Separacion/> 
+      <h1>Ejercicio 11</h1>
+      <Ejercicio11/>
+      <Separacion/> 
+      <h1>Ejercicio 12</h1>
+      <Ejercicio12/>
+      <Separacion/> 
+      <h1>Ejercicio 13 y 14</h1>
+      <Ejercicio1314/>
+      <Separacion/> 
+      <h1>Ejercicio 15</h1>
+      <Ejercicio15/>
     </div>
   );
+}
+
+function Separacion(){
+  return <img
+    src = 'https://colorate.azurewebsites.net/SwatchColor/FFFFFF'
+    height = {300}
+  >
+  </img>
 }
