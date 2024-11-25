31ce4b4e3cae6d0cf03bb6b824cab36495e02160
julia85310 <148855799+julia85310@users.noreply.github.com>
2024-11-25 23:06:42 +0100

    haciendo shoppingList

--- src/app/ShoppingList.js
+++ src/app/ShoppingList.js
@@ -3,6 +3,29 @@ Crea un componente ShoppingList que permita agregar elementos a una lista de com
 eliminar elementos específicos y editar el nombre de un elemento existente.
 Objetivos: useState, manipulación de arrays y edición.
 */
+import {useState} from 'react';
 export default function ShoppingList(){
-    
+    const [elementos, setElementos] = useState([]);
+    const [textInput, setTextInput] = useState('');
+    function handleDeleteElement(indexEliminar){
+        setElementos(elementos.filter((_, index) => index !== indexEliminar));
+    }
+
+    function handleNewElement(){
+        //me he quedado por aqui
+    }
+
+    return <div>
+        {elementos.lenght > 0 &&
+        <ul>
+            {elementos.map((elemento, index) => 
+                <li key={index}>
+                    {elemento}
+                    <button onClick={() => handleDeleteElement(index)}></button>
+                </li>
+            )}
+        </ul>}
+        <input type="text" placeholder="Nuevo elemento" value={textInput} onChange={() => e.target.value}></input>
+        <button onClick={handleNewElement}>Agregar</button>
+    </div>
 }
--- src/app/SurveySimulator.js
+++ src/app/SurveySimulator.js
@@ -4,6 +4,19 @@ que se haga clic en un botón, incrementa un contador asociado a esa respuesta y
 el total de votos por cada opción.
 Objetivos: useState, manejo de varios estados.
 */
+import {useState} from 'react';
 export default function SurveySimulator(){
-    
+    const [votosSi, setVotosSi] = useState(0);
+    const [votosNo, setVotosNo] = useState(0);
+    const [votosTalVez, setVotosTalVez] = useState(0);
+  return <div>
+    <p>Votos para Sí: {votosSi}</p>
+    <p>Votos para No: {votosNo}</p>
+    <p>Votos para Tal Vez: {votosTalVez}</p>
+    <div>
+        <button onClick={() => setVotosSi(votosSi + 1)}>Sí</button>
+        <button onClick={() => setVotosNo(votosNo + 1)}>No</button>
+        <button onClick={() => setVotosTalVez(votosTalVez + 1)}>Tal Vez</button>
+    </div>
+  </div>  
 }
