a89adc9f6f15fb54b9914273f859722bfd57c962
julia85310 <juliarueda85310@gmail.com>
2024-11-26 06:33:14 +0100

    upd ejercicio 10 hecho

--- src/app/GuessTheNumber.js
+++ src/app/GuessTheNumber.js
@@ -3,6 +3,30 @@ Crea un componente GuessTheNumber donde el usuario intente adivinar un número e
 y 100. Muestra si el número ingresado es demasiado alto o bajo, y cuántos intentos lleva.
 Objetivos: useState, validación, lógica condicional.
 */
+import {useState} from 'react';
 export default function GuessTheNumber(){
-    
+  const [theNumber, setTheNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [inputValue, setInputValue] = useState('');
+  const [mensajeEstadoIntento, setMensajeEstadoIntento] = useState('');
+  const [numIntentos, setNumIntentos] = useState(0);
+
+  function tryNumber(){
+    if(inputValue == theNumber){
+      setMensajeEstadoIntento('Has acertado el número');
+    }else if (inputValue > theNumber){
+      setMensajeEstadoIntento('El número que buscas es menor');
+    }else{
+      setMensajeEstadoIntento('El número que buscas es mayor');
+    }
+    setNumIntentos(numIntentos + 1);
+    setInputValue('');
+  }
+
+  return <div>
+    <p>Adivina el número de 1 a 100</p>
+    <input type='number' placeholder='Prueba un número' value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+    <button onClick={tryNumber}>Intentar</button><br></br>
+    <button onClick={() => {setTheNumber(Math.floor(Math.random() * 100) + 1); setMensajeEstadoIntento('');}}>Cambiar número</button>
+    <p>{mensajeEstadoIntento}</p>
+  </div>
 }
--- src/app/UserTable.js
+++ src/app/UserTable.js
@@ -4,6 +4,69 @@ debe tener un nombre, un email y una edad. Agrega un botón para eliminar usuari
 tabla.
 Objetivos: useState, manejo de objetos en arrays.
 */
+import {useState} from 'react';
 export default function UserTable(){
-    
+
+  const[usuarios, setUsuarios] = useState([]);
+  const[agregandoUsuario, setAgregandoUsuario] = useState(false);
+  const[inputName, setInputName] = useState('');
+  const[inputEmail, setInputEmail] = useState('');
+  const[inputEdad, setInputEdad] = useState('');
+
+  function agregarUsuario(){
+    setUsuarios([
+      ...usuarios,
+      {
+        nombre: inputName,
+        email: inputEmail,
+        edad: inputEdad
+      }
+    ]);
+    setInputEdad('');
+    setInputEmail('');
+    setInputName('');
+    setAgregandoUsuario(false);
+  }
+
+  function handleDeleteUser(indexDelete){
+    setUsuarios(usuarios.filter((_, index) => index != indexDelete));
+  }
+
+
+  return <div>
+    <table border={1}>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Email</th>
+          <th>Edad</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          usuarios.map((usuario, index) =>
+            <tr key={index}>
+              <td>{usuario.nombre}</td>
+              <td>{usuario.email}</td>
+              <td>{usuario.edad}</td>
+              <td><button onClick={() => handleDeleteUser(index)}>-</button></td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+
+    {agregandoUsuario ? 
+      <div>
+        <input type='text' value={inputName} placeholder='Nombre' onChange={(e) => setInputName(e.target.value)}></input>
+        <input type='email' value={inputEmail} placeholder='Email' onChange={(e) => setInputEmail(e.target.value)}></input>
+        <input type='number' value={inputEdad} placeholder='Edad' onChange={(e) => setInputEdad(e.target.value)} /*min={0} max={120}*/></input>
+        <button onClick={agregarUsuario}>Confirmar</button>
+      </div>
+      :
+      <button onClick={() => setAgregandoUsuario(true)}>Agregar Usuario</button>
+    }
+
+  </div>
 }
