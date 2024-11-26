ecb450a450cd11a36fa9ec0786dc846ad9c9b10c
julia85310 <juliarueda85310@gmail.com>
2024-11-26 04:27:22 +0100

    ejercicio 8 terminado

--- src/app/MultiCounter.js
+++ src/app/MultiCounter.js
@@ -4,6 +4,42 @@ pantalla. Cada contador debe ser independiente y tener sus propios botones para
 incrementar, decrementar y reiniciar.
 Objetivos: useState, renderización de componentes múltiples.
 */
+import {useState} from 'react';
 export default function MultiCounter(){
-    
+    const[contadores, setContadores] = useState([]);
+
+    function agregarContador(){
+      setContadores([
+        ...contadores,
+        <Counter key={contadores.length}/>
+      ]);
+    }
+
+    return <div>
+      {contadores.map(contador => contador)}
+      <button onClick={agregarContador}>Agregar contador</button>
+    </div>
+}
+
+function Counter(){
+  const [contador, setContador] = useState(0);
+
+  function handleDecrementCounter(){
+    setContador(contador - 1);
+  }
+
+  function handleIncrementCounter(){
+    setContador(contador + 1);
+  }
+
+  function handleResetCounter(){
+    setContador(0);
+  }
+
+  return <div>
+      {contador}
+      <button onClick={handleIncrementCounter}>Incrementar</button>
+      <button onClick={handleDecrementCounter}>Decrementar</button>
+      <button onClick={handleResetCounter}>Reiniciar</button>
+  </div>
 }
